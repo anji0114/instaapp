d68ad3cb9fa8e6a9aab9d6d3b82ab8275be62264
田中杏直 <anji.tnk.140114@gmail.com>
2020-11-30 20:40:20 +0900

    ログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :account_name, uniqueness: true
+  validates :account_name, presence: true
 end
