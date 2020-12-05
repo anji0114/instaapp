6573c4f4f959f0b83b152f90585b95e324eae27f
田中杏直 <anji.tnk.140114@gmail.com>
2020-12-05 12:03:58 +0900

    avatarの表示

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,4 +2,24 @@ class ProfilesController < ApplicationController
   def show
     @account_name = current_user.account_name
   end
+
+  def edit
+    @profile = current_user.profile
+  end
+
+  def update
+    @profile = current_user.prepare_profile
+    @profile.assign_attributes(profile_params)
+    if @profile.save
+      redirect_to profile_path, notice: 'プロフィール更新'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  private
+  def profile_params
+    params.permit(:avatar)
+  end
 end
