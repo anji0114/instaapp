5e8c3546550f03513838bbaffdc2717d98375608
田中杏直 <anji.tnk.140114@gmail.com>
2020-12-10 20:41:42 +0900

    article_controllerの作成

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  has_many_attached :pictures
+  belongs_to :user
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,8 @@ class User < ApplicationRecord
 
   has_one :profile, dependent: :destroy
 
+  has_many :articles, dependent: :destroy
+
   def prepare_profile
     profile || build_profile
   end
