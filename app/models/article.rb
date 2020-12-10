8e81ba88f1eca820ef37aceb507824f6f53abcad
田中杏直 <anji.tnk.140114@gmail.com>
2020-12-10 22:24:15 +0900

    articleのpicturesにvalidate追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,12 @@
 class Article < ApplicationRecord
   has_many_attached :pictures
   belongs_to :user
+  validates :pictures, presence: true
+  validate :validate_pictures
+
+  private
+  def validate_pictures
+    return if pictures.count <= 4
+    errors.add(:pictures, 'You can upload max 4 images')
+  end
 end
