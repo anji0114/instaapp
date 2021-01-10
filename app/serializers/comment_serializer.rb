412f8e698df7020c0a2bfb32533988440a914ac8
田中杏直 <anji.tnk.140114@gmail.com>
2021-01-10 11:10:02 +0900

    commentのapiテスト

--- app/serializers/comment_serializer.rb
+++ app/serializers/comment_serializer.rb
@@ -9,6 +9,10 @@ class CommentSerializer < ActiveModel::Serializer
   end
 
   def avatar_image
-    rails_blob_path(object.user.avatar_image)
+    if object.user.profile&.avatar&.attached?
+      rails_blob_path(object.user.avatar_image)
+    else
+      "/assets/default-avatar-12a3f7379a4333007fa16fb67297f8a31c88580ccdaf11eb0be7323847c5641a.png"
+    end
   end
 end
